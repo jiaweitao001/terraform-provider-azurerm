4c29ecef793daaf616b03c4a3de4c98bff6fe948
Jiawei Tao <jiaweitao@microsoft.com>
2022-10-24 15:49:07 +0800

    tests

--- internal/services/connectedvmware/connected_vmware_cluster_resource_test.go
+++ internal/services/connectedvmware/connected_vmware_cluster_resource_test.go
@@ -1,8 +1,15 @@
 package connectedvmware_test
 
 import (
+	"context"
 	"fmt"
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/connectedvmware/2020-10-01-preview/clusters"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
 	"testing"
 )
 
@@ -15,13 +22,44 @@ func TestAccConnectedVmwareCluster_basic(t *testing.T) {
 	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
 			Config: r.basic(data),
+			Check: acceptance.ComposeTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
 		},
+		data.ImportStep(),
 	})
+}
+
+func (r ConnectedVmwareClusterResource) Exists(ctx context.Context, client *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
+	id, err := clusters.ParseClusterID(state.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := client.ConnectedVmware.ClusterClient.Get(ctx, *id)
+	if err != nil {
+		if response.WasNotFound(resp.HttpResponse) {
+			return utils.Bool(false), nil
+		}
+		return nil, fmt.Errorf("retrieving %s: %+v", *id, err)
+	}
+
+	return utils.Bool(true), nil
 }
 
 func (r ConnectedVmwareClusterResource) basic(data acceptance.TestData) string {
 	template := r.template(data)
+	return fmt.Sprintf(`
+%s
 
+resource "azurerm_connected_vmware_cluster" "test" {
+  name = "acctestcluster%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location = azurerm_resource_group.test.location
+  mo_ref_id = "aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
+  vcenter_id = azurerm_connected_vmware_vcenter.test.id
+}
+`, template, data.RandomInteger)
 }
 
 func (r ConnectedVmwareClusterResource) template(data acceptance.TestData) string {
@@ -31,16 +69,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "test" {
-  name = "acctestRG-%[1]d"
-  location = "%[2]s"
+  name = "acctestRG-%d"
+  location = "%s"
 }
 
-resource "azurerm_connected_vmware_clusters" "test" {
-  name = "acctestcluster%[3]s"
+resource "azurerm_connected_vmware_vcenter" "test" {
+  name = "acctestvcenter-d"
   resource_group_name = azurerm_resource_group.test.name
   location = azurerm_resource_group.test.location
-  mo_ref_id = "aaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee"
-  vcenter_id = 
+  fqdn = "ContosoVMware.contoso.com"
 }
-`)
+`, data.RandomInteger, data.Locations.Primary)
 }
